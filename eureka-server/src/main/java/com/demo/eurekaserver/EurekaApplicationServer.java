f908ce2f0f8c7ead9f96a856b2241ce5155c62c8
jmrodriguez <juan-miguel.rodriguez@soprasteria.com>
2018-02-01 17:25:12 +0100

    git commit -m "First Commit"

---
+++ eureka-server/src/main/java/com/demo/eurekaserver/EurekaApplicationServer.java
@@ -0,0 +1,15 @@
+package com.demo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplicationServer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaApplicationServer.class, args);
+    }
+
+}
