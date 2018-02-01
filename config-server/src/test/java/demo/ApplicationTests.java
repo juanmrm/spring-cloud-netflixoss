893043969da1ed87f4b6f91de03786a5046aaa53
juanmrm <elvior2@hotmail.com>
2018-02-01 21:24:20 +0100

    [FEAT]: Start working on config server integration test.

---
+++ config-server/src/test/java/demo/ApplicationTests.java
@@ -0,0 +1,44 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ConfigServerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+    @LocalServerPort // Same as @Value("${local.server.port}")
+    private int port = 0;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void configurationAvailable() {
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = testRestTemplate.getForEntity("http://localhost:" + port + "/springcloudconfig-client.properties", Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+    @Test
+    public void envPostAvailable() {
+        MultiValueMap<String, String> form = new LinkedMultiValueMap<>();
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> entity = testRestTemplate.postForEntity("http://localhost:" + port + "/admin/env", form, Map.class);
+        assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+}
