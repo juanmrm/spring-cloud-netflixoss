2b1cec942cfcc5516c568ff041b43a1af54f8655
juanmrm <elvior2@hotmail.com>
2018-02-07 13:46:31 +0100

    [CHORE]: Removed dependency.

--- feign-greeting-client/src/main/java/demo/controller/client/IGreetingClient.java
+++ feign-greeting-client/src/main/java/demo/controller/client/IGreetingClient.java
@@ -1,7 +1,10 @@
 package demo.controller.client;
 
-import demo.controller.IGreetingController;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("rest-greeting")
-public interface IGreetingClient extends IGreetingController {}
+public interface IGreetingClient {
+    @RequestMapping("/greeting")
+    String greeting();
+}
